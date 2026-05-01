02db4f4ba8f3e87ea4aa4209ba20c97a470ede8c
VishnuJayadoss <vishnu@innothrust.com>
2026-05-01 18:58:10 +0530

    dashboard

---
+++ app/(components)/dashboard/control/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import DashboardHeader from "@/app/layout/Header";
+import Detail from "./Components/Detail";
+
+export default function Dashboard() {
+    return (
+        <>
+            <DashboardHeader />
+            <Detail />
+        </>
+    );
+}
